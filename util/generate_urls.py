9a8f311539cb3a545839a66dcb50e2502b1a2307
wyfunique <yifanwang2017@gmail.com>
2018-12-12 12:32:07 -0500

    On branch master  Changes to be committed: 	Added a script for generating testing URL list file 	new file:   util/generate_urls.py

---
+++ util/generate_urls.py
@@ -0,0 +1,36 @@
+import os
+import random
+
+class URLGenerator:
+	"""
+		Generate random urls until the url file increases to max_size
+		@filename: path of the file for saving urls
+		@max_num: the maximum number of distinct urls that will be generated
+		@max_size: the maximum size of the file in bytes
+	"""
+	def generateURLs(self, filename, max_num, max_size):
+		# First, generate max_num distinct URLs
+		urls = set(); 
+		while len(urls) < max_num:
+			urls.add("http://www.url.com/" + ("%f" % random.random()) * random.choice(range(5)))
+		urls = list(urls)
+		# Second, fill the file using URLs chosen from the set above
+		f = open(filename, 'w+')
+		f.write('')
+		f.close()
+		f = open(filename, 'a')
+		while (os.path.getsize(filename) < max_size):
+			f.write(random.choice(urls) + '\n')
+		f.close()
+	
+if __name__ == "__main__":
+	byte = 1
+	KB = 1000 * byte
+	MB = 1000 * KB
+	GB = 1000 * MB
+	
+	filename = "urls.txt"
+	max_num = 1000
+	max_size = 1 * GB
+	gen = URLGenerator()
+	gen.generateURLs(filename, max_num, max_size)
